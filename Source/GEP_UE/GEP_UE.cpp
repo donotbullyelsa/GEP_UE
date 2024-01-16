0bc84ce56878cb0b3f234f776c6b563931d3d51e
Yat Long Chan <yat3.chan@live.uwe.ac.uk>
2024-01-17 03:53:18 +0800

    .

---
+++ Source/GEP_UE/GEP_UE.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GEP_UE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GEP_UE, "GEP_UE" );
---
+++ Source/GEP_UE/Public/ItemNone.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class GEP_UE_API ItemNone
+{
+public:
+	ItemNone();
+	~ItemNone();
+};
